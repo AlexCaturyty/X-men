157b25c8ad4bdd411dc2012edbe044f90730d7a4
AlexCaturyty <olucascavalcanti@gmail.com>
2023-07-10 14:15:06 -0300

    Ajustes local das fotos

--- src/js/script.js
+++ src/js/script.js
@@ -1,48 +1,48 @@
 function ciclope(){
 
-    document.querySelector(".personagem-grande").src = "/src/img/card-ciclope.png";
+    document.querySelector(".personagem-grande").src = "./src/img/card-ciclope.png";
     document.getElementById("nome-personagem").innerHTML = "CICLOPE";
     document.getElementById("descricao-personagem").innerHTML = " Ciclope tem o poder de disparar rajadas opticas por um acidente que aconteceu com ele quando criança, mas não consegue controlá-los"
 }
 
 function jeangrey(){
-    document.querySelector(".personagem-grande").src = "/src/img/card-jean-grey.png";
+    document.querySelector(".personagem-grande").src = "./src/img/card-jean-grey.png";
     document.getElementById("nome-personagem").innerHTML = "JEAN GRAY";
     document.getElementById("descricao-personagem").innerHTML = " Jean Grey possui o poder de manipular e canalizar a energia psíquica graças a um incidente traumático em sua infância, porém, luta para controlá-lo plenamente."
 
 }
 function lincenegra(){
-    document.querySelector(".personagem-grande").src = "/src/img/card-lince-negra.png";
+    document.querySelector(".personagem-grande").src = "./src/img/card-lince-negra.png";
     document.getElementById("nome-personagem").innerHTML = "LINCE NEGRA";
     document.getElementById("descricao-personagem").innerHTML = " Kitty é uma mutante com a capacidade de passar pela matéria sólida, passando suas partículas atômicas pelos espaços entre os átomos do objeto pelo qual ela está se movendo"
 
 }
 function magneto(){
-    document.querySelector(".personagem-grande").src = "/src/img/card-magneto.png";
+    document.querySelector(".personagem-grande").src = "./src/img/card-magneto.png";
     document.getElementById("nome-personagem").innerHTML = "MAGNETO";
     document.getElementById("descricao-personagem").innerHTML = "  Magneto é um mutante com enormes poderes de manipulação de campos magnéticos, além de controlar qualquer tipo de metal, incluindo o Adamantium"
 
 }
 function noturno(){
-    document.querySelector(".personagem-grande").src = "/src/img/card-noturno.png";
+    document.querySelector(".personagem-grande").src = "./src/img/card-noturno.png";
     document.getElementById("nome-personagem").innerHTML = "NOTURNO";
     document.getElementById("descricao-personagem").innerHTML = " Noturno tem a capacidade se teleportar. Ele se desloca por meio da Dimensão do Enxofre, viajando por ela e reaparecendo depois em sua própria dimensão."
 
 }
 function tempestade(){
-    document.querySelector(".personagem-grande").src = "/src/img/card-tempestade.png";
+    document.querySelector(".personagem-grande").src = "./src/img/card-tempestade.png";
     document.getElementById("nome-personagem").innerHTML = "TEMPESTADE";
     document.getElementById("descricao-personagem").innerHTML = " Tempestade consegue alterar sua percepção visual para ver o universo como padrões de energia, detectando o fluxo de energia eletromagnética, termal ou cinética e o que pode estar causando um fenômeno climático"
 
 }
 function vampira(){
-    document.querySelector(".personagem-grande").src = "/src/img/card-vampira.png";
+    document.querySelector(".personagem-grande").src = "./src/img/card-vampira.png";
     document.getElementById("nome-personagem").innerHTML = "VAMPIRA";
     document.getElementById("descricao-personagem").innerHTML = " Vampira tem habilidade de absorver a vitalidade, memórias, habilidades e poderes de outros seres vivos através do contato com a pele"
 
 }
 function wolverine(){
-    document.querySelector(".personagem-grande").src = "/src/img/card-wolverine.png";
+    document.querySelector(".personagem-grande").src = "./src/img/card-wolverine.png";
     document.getElementById("nome-personagem").innerHTML = "WOLVERINE";
     document.getElementById("descricao-personagem").innerHTML = " Ele possui a capacidade de regeneração acelerada e garras retráteis de adamantium, resultado de um experimento científico que o tornou praticamente imortal. "
 
